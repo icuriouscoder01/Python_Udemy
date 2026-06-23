00ec109413094646e00a15b929444dd79ec364e5
Arnav Gupta <iarnavgupta01@gmail.com>
2026-06-24 04:04:56 +0530

    Adds New Project.

---
+++ Inverted Dict/inv_dict.py
@@ -0,0 +1,11 @@
+original = {'a': 1, 'b': 2, 'c': 1, 'd': 2, 'e': 3, 'f': 2}
+
+inverted = {}
+
+for key, value in original.items():
+    if value not in inverted:
+        inverted[value] = {key}
+    else:
+        inverted[value].add(key)
+
+print(f"Inverted Dictionary: {inverted}")
