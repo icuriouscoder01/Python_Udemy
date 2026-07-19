24bf7c9107392f8645bd0edd700ed330e43c989d
Arnav Gupta <iarnavgupta01@gmail.com>
2026-07-20 01:05:30 +0530

    Last commit

---
+++ Class For Circle/circle.py
@@ -0,0 +1,17 @@
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius  # Store the radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+        
+    def perimeter(self):
+       return 2 * math.pi * self.radius       
+       
+radius = 5.0
+
+
+c1 = Circle(radius)
+print(f"Area: {c1.area():.2f}")
+print(f"Perimeter: {c1.perimeter():.2f}")
